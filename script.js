5adf6fe0a1006b4e9c853d992c7293a22ec4fbae
Ahsankt <ahsankt24@gmail.com>
2025-03-24 02:59:54 +0000

    Improvement in CSS

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // script.js
 
-function showProductPage(productType) {
-    if (productType === 'Acoustic Guitars') {
-        window.location.href = 'acoustic-guitars.html';
-    } else if (productType === 'Electric Guitars') {
-        window.location.href = 'electric-guitars.html';
-    } else if (productType === 'Accessories') {
-        window.location.href = 'accessories.html';
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Website Loaded");
+
+    // Check URL to apply correct background class
+    if (window.location.href.includes("acoustic-guitars.html")) {
+        document.body.className = "acoustic";
+    } else if (window.location.href.includes("electric-guitars.html")) {
+        document.body.className = "electric";
+    }
 });
+
+// Function to navigate and set background dynamically
+function changeBackground(type) {
+    if (type === "acoustic") {
+        document.body.className = "acoustic";
+        window.location.href = "acoustic-guitars.html";
+    } else if (type === "electric") {
+        document.body.className = "electric";
+        window.location.href = "electric-guitars.html";
+    }
+}
